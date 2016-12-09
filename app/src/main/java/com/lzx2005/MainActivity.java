760589c88905b45afddf56abccd738d9703c2212
lizhengxian <lizhengxian@yscredit.com>
2016-12-09 17:36:10 +0800

    集成访问网络的代码

--- app/src/main/java/com/lzx2005/MainActivity.java
+++ app/src/main/java/com/lzx2005/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lzx2005;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
     private EditText host;
@@ -31,6 +41,49 @@ public class MainActivity extends AppCompatActivity {
 
             Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
             toast.show();
+
+            CallAPI callAPI = new CallAPI();
+            callAPI.execute("http://112.74.33.145:8050/find?url=http://www.lzx2005.com");
+        }
+    }
+
+
+    private class CallAPI extends AsyncTask<String, String, String> {
+        public CallAPI() {
+            //set context variables if required
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0]; // URL to call
+            String resultToDisplay = "";
+            InputStream in = null;
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                in = new BufferedInputStream(urlConnection.getInputStream());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return e.getMessage();
+            }
+            try {
+                resultToDisplay = IOUtils.toString(in, "UTF-8");
+                //to [convert][1] byte stream to a string
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return resultToDisplay;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("result",result);
+            //query.setText("test");
+            //Update the UI
         }
     }
 }
---
+++ app/src/main/java/com/lzx2005/tool/CallAPI.java
@@ -0,0 +1,11 @@
+package com.lzx2005.tool;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.commons.io.IOUtils;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
